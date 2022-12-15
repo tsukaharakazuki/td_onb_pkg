e556a05097cd76d634d5d0ef5a651440dd419c5c
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2022-12-15 12:01:54 +0900

    Add files via upload

---
+++ agg_master_segment/query/p21_master_table_webapp.sql
@@ -0,0 +1,12 @@
+SELECT
+  behavior_type ,
+  td_ms_id ,
+  MAX_BY(ifa, time) AS ifa ,
+  MAX_BY(ifa_type, time) AS ifa_type ,
+  ARRAY_AGG(DISTINCT ifa) FILTER(WHERE ifa IS NOT NULL) AS ifas ,
+  MAX_BY(user_id, time) AS user_id ,
+  ARRAY_AGG(DISTINCT user_id) FILTER(WHERE user_id IS NOT NULL) AS user_ids 
+FROM 
+  ${media[params].output_db}.ms_behavior_${media[params].media_name}
+GROUP BY 
+  1,2
---
+++ agg_master_segment/query/p22_master_table_web.sql
@@ -0,0 +1,10 @@
+SELECT
+  behavior_type ,
+  td_ms_id ,
+  MAX_BY(td_client_id, time) AS td_client_id ,
+  MAX_BY(user_id, time) AS user_id ,
+  ARRAY_AGG(DISTINCT user_id) FILTER(WHERE user_id IS NOT NULL) AS user_ids 
+FROM 
+  ${media[params].output_db}.ms_behavior_${media[params].media_name}
+GROUP BY 
+  1,2
