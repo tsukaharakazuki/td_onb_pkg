6360b93a7193d6f279a0189fc33e8cfbebf8df61
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2023-05-11 07:54:12 +0900

    Create inquiry_pred_data.sql

---
+++ ml_lookalike/sql/inquiry_pred_data.sql
@@ -0,0 +1,28 @@
+WITH t0 AS (
+  SELECT
+    table_schema AS db , 
+    table_name AS tbl ,
+    REGEXP_REPLACE(table_name,'^predicted_','') AS segment_tbl
+  FROM
+    information_schema.tables
+  WHERE
+    table_schema = '${database}'
+    AND REGEXP_LIKE(table_name,'^predicted_')
+)
+
+SELECT
+  a.* ,
+  b.segment_name
+FROM
+  t0 a
+  INNER JOIN (
+    SELECT
+      segment_tbl ,
+      MAX_BY(segment_name,time) AS segment_name
+    FROM
+      ${target_db}.cdp_ml_target
+    GROUP BY
+      1
+  ) b
+  ON
+    a.segment_tbl = b.segment_tbl
