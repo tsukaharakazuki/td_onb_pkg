cecb5b57aad239f772c1e046c245f8a0fba80562
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2023-07-08 23:46:19 +0900

    Update p22_master_table_web.sql

--- agg_master_segment/query/p22_master_table_web.sql
+++ agg_master_segment/query/p22_master_table_web.sql
@@ -1,6 +1,7 @@
 SELECT
   behavior_type ,
   td_ms_id ,
+  MAX_BY(td_cookie, time) AS td_cookie ,
   MAX_BY(td_client_id, time) AS td_client_id 
   --, MAX_BY(user_id, time) AS user_id ,
   --ARRAY_AGG(DISTINCT user_id) FILTER(WHERE user_id IS NOT NULL) AS user_ids 
