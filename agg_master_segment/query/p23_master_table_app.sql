370fb62a926ea33a5339ee26de88bc86b06798cb
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2023-07-16 10:28:55 +0900

    Update p23_master_table_app.sql

--- agg_master_segment/query/p23_master_table_app.sql
+++ agg_master_segment/query/p23_master_table_app.sql
@@ -1,6 +1,7 @@
 SELECT
   behavior_type ,
   td_ms_id ,
+  td_ms_id_type ,
   MAX_BY(ifa, if(ifa is null,null,time)) AS ifa ,
   MAX_BY(ifa_type, if(ifa_type is null,null,time)) AS ifa_type ,
   ARRAY_AGG(DISTINCT ifa) FILTER(WHERE ifa IS NOT NULL) AS ifas 
