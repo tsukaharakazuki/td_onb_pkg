9df85c660d3ce754e52df9d85e9829117950d511
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2023-07-16 10:28:20 +0900

    Update p21_master_table_webapp.sql

--- agg_master_segment/query/p21_master_table_webapp.sql
+++ agg_master_segment/query/p21_master_table_webapp.sql
@@ -1,6 +1,7 @@
 SELECT
   behavior_type ,
   td_ms_id ,
+  td_ms_id_type ,
   MAX_BY(ifa, if(ifa is null,null,time)) AS ifa ,
   MAX_BY(ifa_type, if(ifa_type is null,null,time)) AS ifa_type ,
   ARRAY_AGG(DISTINCT ifa) FILTER(WHERE ifa IS NOT NULL) AS ifas ,
@@ -12,4 +13,4 @@ SELECT
 FROM 
   ${media[params].output_db}.ms_behavior_${media[params].media_name}
 GROUP BY 
-  1,2
+  1,2,3
