0c9b9aedb08caede93d307e4f75d0e401112926c
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2023-08-14 12:12:49 +0900

    Update p21_master_table_webapp.sql

--- agg_master_segment/query/p21_master_table_webapp.sql
+++ agg_master_segment/query/p21_master_table_webapp.sql
@@ -6,10 +6,8 @@ SELECT
   MAX_BY(ifa_type, if(ifa_type is null,null,time)) AS ifa_type ,
   ARRAY_AGG(DISTINCT ifa) FILTER(WHERE ifa IS NOT NULL) AS ifas ,
   MAX_BY(td_cookie, if(td_cookie is null,null,time)) AS td_cookie ,
-  MAX_BY(td_ssc_id, if(td_ssc_id is null,null,time)) AS td_ssc_id ,
-  ARRAY_AGG(DISTINCT td_ssc_id) FILTER(WHERE td_ssc_id IS NOT NULL) AS td_ssc_ids 
-  --, MAX_BY(user_id, time) AS user_id ,
-  --ARRAY_AGG(DISTINCT user_id) FILTER(WHERE user_id IS NOT NULL) AS user_ids 
+  MAX_BY(td_client_id, if(td_client_id is null,null,time)) AS td_client_id ,
+  MAX_BY(user_id, time) AS user_id 
 FROM 
   ${media[params].output_db}.ms_behavior_${media[params].media_name}
 GROUP BY 
