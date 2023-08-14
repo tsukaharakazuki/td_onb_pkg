dbefe3ca67f5c8cb0906c0b1677fbea1ae5a27d6
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2023-08-14 12:11:39 +0900

    Update p23_master_table_app.sql

--- agg_master_segment/query/p23_master_table_app.sql
+++ agg_master_segment/query/p23_master_table_app.sql
@@ -1,12 +1,10 @@
 SELECT
   behavior_type ,
   td_ms_id ,
-  td_ms_id_type ,
   MAX_BY(ifa, if(ifa is null,null,time)) AS ifa ,
   MAX_BY(ifa_type, if(ifa_type is null,null,time)) AS ifa_type ,
-  ARRAY_AGG(DISTINCT ifa) FILTER(WHERE ifa IS NOT NULL) AS ifas 
-  --, MAX_BY(user_id, time) AS user_id ,
-  --ARRAY_AGG(DISTINCT user_id) FILTER(WHERE user_id IS NOT NULL) AS user_ids 
+  ARRAY_AGG(DISTINCT ifa) FILTER(WHERE ifa IS NOT NULL) AS ifas , 
+  MAX_BY(user_id, time) AS user_id 
 FROM 
   ${media[params].output_db}.ms_behavior_${media[params].media_name}
 GROUP BY 
