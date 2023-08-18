e3cac01f069adf509e7dd60951857ad188cdba9f
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2023-08-18 10:20:57 +0900

    Update p60_ms_behavior_last_access.sql

--- agg_master_segment/query/p60_ms_behavior_last_access.sql
+++ agg_master_segment/query/p60_ms_behavior_last_access.sql
@@ -24,8 +24,8 @@ WITH t0 AS (
   SELECT
     td_ms_id ,
     media_name ,
-    MAX(time) AS last_access_unix ,
-    TD_TIME_FORMAT(MAX(time),'yyyy-MM-dd HH:mm:ss','JST') AS last_access
+    MAX(last_access_unix) AS last_access_unix ,
+    TD_TIME_FORMAT(MAX(last_access_unix),'yyyy-MM-dd HH:mm:ss','JST') AS last_access
   FROM
     t0
   GROUP BY
