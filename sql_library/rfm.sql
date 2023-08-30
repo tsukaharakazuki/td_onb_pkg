a620db09d8cd257e392f7f44cbbe1b37139ed714
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2023-08-30 15:26:37 +0900

    Update rfm.sql

--- sql_library/rfm.sql
+++ sql_library/rfm.sql
@@ -2,7 +2,11 @@ SELECT
   key_id ,
   CAST(TRUNCATE((MAX(time)- TO_UNIXTIME(now()))/ 86400) AS INT) AS recency,
   COUNT(DISTINCT TD_TIME_FORMAT(time,'yyyy-MM-dd','JST')) AS frequency,
-  SUM(amount) AS monetary
+  SUM(amount) AS monetary ,
+  MIN(time) AS first_order_unix  ,
+  MAX(time) AS last_order_unix ,
+  TD_TIME_FORMAT(MIN(time),'yyyy-MM-dd HH:mm:ss','JST') AS first_order_date ,
+  TD_TIME_FORMAT(MAX(time),'yyyy-MM-dd HH:mm:ss','JST') AS last_order_date 
 FROM
   db.tbl
 GROUP BY
