a937fe927805bdef9f8b3a53cb308d2cb6b6769b
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2023-11-15 14:38:05 +0900

    Update mst_tbl_cookie.sql

--- agg_master_segment/comp/mst_tbl_cookie.sql
+++ agg_master_segment/comp/mst_tbl_cookie.sql
@@ -1,10 +1,11 @@
 SELECT
   behavior_type ,
-  td_cookie AS td_ms_id ,
-  user_id ,
-  MAX_BY(td_client_id, time) AS td_client_id 
-  --, MAX_BY(user_id, time) AS user_id ,
-  --ARRAY_AGG(DISTINCT user_id) FILTER(WHERE user_id IS NOT NULL) AS user_ids 
+  td_ms_id,
+  td_ms_id_type ,
+  MAX(user_id_comp) FILTER(WHERE user_id_comp IS NOT NULL) AS user_id ,
+  MAX_BY(td_cookie, if(td_cookie is null,null,time)) AS td_cookie ,
+  ARRAY_AGG(DISTINCT td_cookie) FILTER(WHERE td_cookie IS NOT NULL) AS td_cookies ,
+  MAX_BY(td_client_id, if(td_client_id is null,null,time)) AS td_client_id 
 FROM 
   ${media[params].output_db}.ms_behavior_${media[params].media_name}
 GROUP BY 
