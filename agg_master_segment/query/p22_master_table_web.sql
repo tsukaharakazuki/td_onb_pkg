f7466d0a25d9fa2da02f9ab7b8c3c3737245eba7
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2023-11-15 10:14:29 +0900

    Update p22_master_table_web.sql

--- agg_master_segment/query/p22_master_table_web.sql
+++ agg_master_segment/query/p22_master_table_web.sql
@@ -1,9 +1,11 @@
 SELECT
   behavior_type ,
-  td_ms_id ,
+  td_ms_id,
+  td_ms_id_type ,
+  MAX(user_id_comp) FILTER(WHERE user_id_comp IS NOT NULL) AS user_id ,
   MAX_BY(td_cookie, time) AS td_cookie ,
-  MAX_BY(td_client_id, time) AS td_client_id , 
-  MAX_BY(user_id, time) AS user_id 
+  ARRAY_AGG(DISTINCT td_cookie) FILTER(WHERE td_cookie IS NOT NULL) AS td_cookies ,
+  MAX_BY(td_client_id, time) AS td_client_id 
 FROM 
   ${media[params].output_db}.ms_behavior_${media[params].media_name}
 GROUP BY 
