ef987d90cdb4599d2bfaed2b51b56988194fc49b
Tsukahara-Kazuki <33442939+Tsukahara-Kazuki@users.noreply.github.com>
2023-11-29 09:22:56 +0900

    Update p21_master_table_webapp.sql

--- agg_master_segment/query/p21_master_table_webapp.sql
+++ agg_master_segment/query/p21_master_table_webapp.sql
@@ -7,7 +7,7 @@ SELECT
   ARRAY_AGG(DISTINCT ifa) FILTER(WHERE ifa IS NOT NULL) AS ifas ,
   MAX_BY(td_cookie, if(td_cookie is null,null,time)) AS td_cookie ,
   MAX_BY(td_client_id, if(td_client_id is null,null,time)) AS td_client_id ,
-  MAX_BY(user_id, time) AS user_id 
+  MAX(user_id_comp) FILTER(WHERE user_id_comp IS NOT NULL) AS user_id 
 FROM 
   ${media[params].output_db}.ms_behavior_${media[params].media_name}
 GROUP BY 
