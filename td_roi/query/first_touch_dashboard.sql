26edb44f5eb16fec71ee71f2506b38fb273f0201
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2023-12-01 17:25:45 +0900

    Add files via upload

---
+++ td_roi/query/first_touch_dashboard.sql
@@ -0,0 +1,18 @@
+SELECT
+  TD_TIME_FORMAT(time_purchace,'yyyy-MM-dd','JST') AS purchace_date ,
+  TD_TIME_FORMAT(time_purchace,'yyyy-MM','JST') AS purchace_month ,
+  TD_TIME_FORMAT(time_purchace,'yyyy','JST') AS purchace_year ,
+  td_source ,
+  td_medium ,
+  td_campaign ,
+  td_smc ,
+  SUM(amount) AS amount_total ,
+  COUNT(DISTINCT user_id) AS uu_date
+FROM
+  roi_first_touch_yesterday
+GROUP BY
+  time_purchace ,
+  td_source ,
+  td_medium ,
+  td_campaign ,
+  td_smc   
