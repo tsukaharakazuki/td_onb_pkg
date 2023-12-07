c89bd8b7438c6985b47cfd93958612791983de30
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2023-12-07 09:26:04 +0900

    Update p31_attribute_engagement_score.sql

--- agg_master_segment/query/p31_attribute_engagement_score.sql
+++ agg_master_segment/query/p31_attribute_engagement_score.sql
@@ -1,24 +1,36 @@
 WITH base AS (
   SELECT
     td_ms_id ,
-    CAST(TRUNCATE((MAX(time)- TO_UNIXTIME(now()))/ 86400) AS INT) AS recency ,
-    COUNT(DISTINCT TD_TIME_FORMAT(time,'yyyy-MM-dd','JST')) AS frequency ,
-    COUNT(DISTINCT engagement_vols) AS volume
+    MIN_BY(utm_source,num) AS td_source_first ,
+    MIN_BY(utm_medium,num) AS td_medium_first ,
+    MIN_BY(utm_campaign,num) AS td_campaign_first ,
+    MAX_BY(user_id_comp, if(user_id_comp is null,null,time)) AS user_id ,
+    CAST((TD_DATE_TRUNC('day',unix_timestamp()) - TD_DATE_TRUNC('day',MIN(time))) / 86400 AS INT) AS td_progress_web ,
+    CAST((TD_DATE_TRUNC('day',MAX(time)) - TD_DATE_TRUNC('day',unix_timestamp())) / 86400 AS INT) AS td_recency_web ,
+    COUNT(DISTINCT TD_TIME_FORMAT(time,'yyyy-MM-dd','JST')) AS td_frequency_web ,
+    COUNT(DISTINCT engagement_vols) AS td_volume_web ,
+    MIN(time) AS time
     ${(Object.prototype.toString.call(media[params].add_engagement_calc.sql) === '[object Array]')?','+media[params].add_engagement_calc.sql.join():''}
-  FROM
-    ${media[params].output_db}.ms_behavior_${media[params].media_name}
+  FROM (
+    SELECT
+      * ,
+      ROW_NUMBER() OVER (PARTITION BY td_ms_id ORDER BY time ASC) AS num
+    FROM
+      ${media[params].output_db}.ms_behavior_${media[params].media_name}
+  ) t
   GROUP BY
     1
 )
-
+  
+-- DIGDAG_INSERT_LINE
 SELECT
   * ,
-  recency * -1 AS recency_pn ,
+  td_recency_web * -1 AS td_recency_web_pn ,
   ntile(10) OVER (ORDER BY engagement_score DESC) AS decile
 FROM (
   SELECT
     * ,
-    log10(frequency * SQRT(volume)* (${td.last_results.progress} + recency + 1)) AS engagement_score
+    log10(td_frequency_web * SQRT(td_volume_web)* (${td.last_results.progress} + td_recency_web + 1)) AS engagement_score
   FROM
     base
-)
+) t
