33f40b65f4f71118a3bdea3a42af48a4d61e36e2
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2023-12-18 23:31:43 +0900

    Update l2_new_visitor_dashbord.sql

--- new_visitors_analytics/query/l2_new_visitor_dashbord.sql
+++ new_visitors_analytics/query/l2_new_visitor_dashbord.sql
@@ -1,7 +1,7 @@
 SELECT
   TD_TIME_FORMAT(time,'yyyy','JST') AS year ,
-  TD_TIME_FORMAT(time,'yyyy-MM','JST') AS month ,
-  TD_TIME_FORMAT(time,'yyyy-MM-dd','JST') AS date 
+  TD_TIME_FORMAT(time,'yyyyMM','JST') AS month ,
+  TD_TIME_FORMAT(time,'yyyyMMdd','JST') AS date 
   ${(Object.prototype.toString.call(media[params].add_columns) === '[object Array]')?','+media[params].add_columns.join():''}
   , td_source ,
   td_medium ,
@@ -11,8 +11,8 @@ FROM
   l1_new_visitor
 GROUP BY
   TD_TIME_FORMAT(time,'yyyy','JST') ,
-  TD_TIME_FORMAT(time,'yyyy-MM','JST') ,
-  TD_TIME_FORMAT(time,'yyyy-MM-dd','JST') 
+  TD_TIME_FORMAT(time,'yyyyMM','JST') ,
+  TD_TIME_FORMAT(time,'yyyyMMdd','JST') 
   ${(Object.prototype.toString.call(media[params].add_columns) === '[object Array]')?','+media[params].add_columns.join():''}
   , td_source ,
   td_medium ,
