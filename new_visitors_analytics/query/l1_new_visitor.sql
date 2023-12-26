4e574db83c388ff11c507eb7ccfabae555a3b88f
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2023-12-26 15:21:02 +0900

    Update l1_new_visitor.sql

--- new_visitors_analytics/query/l1_new_visitor.sql
+++ new_visitors_analytics/query/l1_new_visitor.sql
@@ -2,11 +2,15 @@ SELECT
   ${media[params].key_id} 
   ${(Object.prototype.toString.call(media[params].add_columns) === '[object Array]')?','+media[params].add_columns.join():''}
   , MIN(time) AS time ,
-  MIN_BY(td_source,time) AS td_source ,
-  MIN_BY(td_medium,time) AS td_medium ,
-  MIN_BY(td_campaign,time) AS td_campaign
-FROM
-  l1_new_visitor_tmp
-GROUP BY
-  ${media[params].key_id} 
-  ${(Object.prototype.toString.call(media[params].add_columns) === '[object Array]')?','+media[params].add_columns.join():''}
+  td_source ,
+  td_medium ,
+  td_campaign
+FROM (
+  SELECT
+    * ,
+    ROW_NUMBER() OVER (PARTITION BY ${media[params].key_id} ORDER BY time ASC) AS dum
+  FROM
+    l1_new_visitor_tmp
+) t
+WHERE
+  dum = 1
