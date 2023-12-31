974a998f624626c0505a80bb55b8124a63f9d5a0
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2023-12-31 21:27:56 +0900

    Add files via upload

---
+++ td_roi/bulk/dashboard_roi.sql
@@ -0,0 +1,20 @@
+SELECT
+  TD_TIME_FORMAT(time_purchace,'yyyy-MM-dd','JST') AS purchace_date ,
+  TD_TIME_FORMAT(time_purchace,'yyyy-MM','JST') AS purchace_month ,
+  TD_TIME_FORMAT(time_purchace,'yyyy','JST') AS purchace_year ,
+  td_source ,
+  td_medium ,
+  td_campaign ,
+  td_smc ,
+  SUM(amount_first_touch) AS amount_first_touch ,
+  SUM(amount_last_touch) AS amount_last_touch ,
+  SUM(amount_multi_touch) AS amount_multi_touch ,
+  SUM(amount_multi_touch_split) AS amount_multi_touch_split
+FROM
+  dist_roi
+GROUP BY
+  time_purchace ,
+  td_source ,
+  td_medium ,
+  td_campaign ,
+  td_smc
---
+++ td_roi/bulk/dist_roi.sql
@@ -0,0 +1,27 @@
+WITH t0 AS (
+  SELECT
+    TD_SESSIONIZE_WINDOW(time, (60*60*24*${roi_judge_date})) OVER (PARTITION BY ${pos.sessionize_col} ORDER BY time_inflow) AS p_session_id ,
+    *
+  FROM
+    roi_behabior_purchace
+)
+
+, t1 AS (
+  SELECT
+    ROW_NUMBER() OVER (PARTITION BY p_session_id ORDER BY time_inflow ASC) AS first_touch ,
+    ROW_NUMBER() OVER (PARTITION BY p_session_id ORDER BY time_inflow DESC) AS last_touch ,
+    1 AS multi_touch ,
+    1.0 / (COUNT(*) OVER (PARTITION BY p_session_id)) AS multi_touch_split ,
+    *
+  FROM
+    t0
+)
+
+SELECT
+  amount * IF(first_touch = 1,first_touch,NULL) AS amount_first_touch ,
+  amount * IF(last_touch = 1,last_touch,NULL) AS amount_last_touch ,
+  amount * multi_touch AS amount_multi_touch ,
+  amount * multi_touch_split AS amount_multi_touch_split ,
+  *
+FROM  
+  t1
