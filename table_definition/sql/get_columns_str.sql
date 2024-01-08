5a09def35c1e4c95710f7d3a8bec8718fc4dc4c1
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2024-01-08 13:51:51 +0900

    Update get_columns_str.sql

--- table_definition/sql/get_columns_str.sql
+++ table_definition/sql/get_columns_str.sql
@@ -3,7 +3,7 @@ SELECT
   ,ARRAY_JOIN(array_sort(array_distinct(ARRAY_AGG(column_name))), ',') AS sample_columns
   ,ARRAY_JOIN(array_sort(array_distinct(ARRAY_AGG('MAX(' || column_name || ') AS ' || column_name))), ', ') AS max_col_name_columns
 FROM
-  tmp_columns_${flag}_${data_type}
+  tmp_columns_${flag}_${data_type.col}
 WHERE
   database_name = '${td.each.database_name}'
   AND table_name = '${td.each.table_name}'
