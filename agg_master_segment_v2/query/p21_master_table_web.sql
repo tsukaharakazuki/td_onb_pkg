aff7844a0e3c3dbdfae0654d7e95176e93e8e0b7
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2024-04-06 17:21:34 +0900

    Update p21_master_table_web.sql

--- agg_master_segment_v2/query/p21_master_table_web.sql
+++ agg_master_segment_v2/query/p21_master_table_web.sql
@@ -2,7 +2,7 @@ SELECT
   behavior_type ,
   td_ms_id,
   td_ms_id_type ,
-  MAX(user_id_comp) FILTER(WHERE user_id_comp IS NOT NULL) AS user_id ,
+  MAX(user_id_comp) AS user_id ,
   MAX_BY(td_cookie, if(td_cookie is null,null,time)) AS td_cookie ,
   COLLECT_SET(td_cookie) AS td_cookies ,
   MAX_BY(td_client_id, if(td_client_id is null,null,time)) AS td_client_id ,
