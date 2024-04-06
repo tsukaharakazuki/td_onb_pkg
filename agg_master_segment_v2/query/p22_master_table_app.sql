6dff18ef9c7cf58f628caeacdd3edef9334c3e43
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2024-04-06 17:21:53 +0900

    Update p22_master_table_app.sql

--- agg_master_segment_v2/query/p22_master_table_app.sql
+++ agg_master_segment_v2/query/p22_master_table_app.sql
@@ -4,7 +4,7 @@ SELECT
   MAX_BY(ifa, if(ifa is null,null,time)) AS ifa ,
   MAX_BY(ifa_type, if(ifa_type is null,null,time)) AS ifa_type ,
   COLLECT_SET(ifa) AS ifas , 
-  MAX_BY(user_id, time) AS user_id ,
+  MAX(user_id) AS user_id ,
   MAX(time) AS time
 FROM 
   ${media[params].output_db}.ms_behavior_${media[params].media_name}
