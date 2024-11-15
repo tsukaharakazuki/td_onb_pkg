f9949c1037d92d955b93d78242c47632f5cc501a
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2024-11-15 10:47:04 +0900

    Update inquiry_pred_data.sql

--- ml_lookalike/sql/inquiry_pred_data.sql
+++ ml_lookalike/sql/inquiry_pred_data.sql
@@ -17,7 +17,9 @@ FROM
   t0 a
   INNER JOIN (
     SELECT
-      segment_tbl ,
+      REGEXP_REPLACE(
+        LOWER(segment_tbl), '\s',''
+      ) AS segment_tbl ,
       MAX_BY(segment_name,time) AS segment_name
     FROM
       ${cdp[params].db}.${cdp[params].tbl}
