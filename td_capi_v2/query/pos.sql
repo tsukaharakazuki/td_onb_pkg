ea12cf8b5530e66b855ffbf71170ee0e33f9297c
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2025-01-10 14:38:56 +0900

    Update pos.sql

--- td_capi_v2/query/pos.sql
+++ td_capi_v2/query/pos.sql
@@ -12,13 +12,13 @@ SELECT
   ${brand[params].pos.user_id} AS uid ,
   '${brand[params].pos.url}' AS url ,
   '${brand[params].pos.ip}' AS ip ,
-  ${brand[params].pos.user_agent} AS user_agent ,
+  '${brand[params].pos.user_agent}' AS user_agent ,
   '${brand[params].pos.title}' AS title ,
   IF(${brand[params].pos.order_id} is not NULL ,${brand[params].pos.order_id},CAST(NULL AS VARCHAR)) AS order_id ,
   ${brand[params].pos.amount} AS amount ,
   IF(
     REGEXP_LIKE(${brand[params].pos.order_date_time},'^\d$') ,
-    REGEXP_LIKE(${brand[params].pos.order_date_time} ,
+    ${brand[params].pos.order_date_time} ,
     TD_TIME_PARSE(REGEXP_LIKE(${brand[params].pos.order_date_time},'JST')
   ) AS time
 FROM
