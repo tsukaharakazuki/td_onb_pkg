b40369fe28987a0a154883e0be5d697b98f7dcc4
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2025-08-26 15:16:52 +0900

    Update output.sql

--- td_roi_analytics/queries/output.sql
+++ td_roi_analytics/queries/output.sql
@@ -1,9 +1,9 @@
 SELECT
   TD_TIME_FORMAT(time,'yyyy/MM/dd','JST') AS date ,
-  td_source_first ,
-  td_medium_recent ,
-  td_campaign_first ,
-  IF(REGEXP_LIKE(td_campaign_first,'${td_flag_params}'),1,0) AS td_flag ,
+  td_source_${flag} AS td_source ,
+  td_medium_${flag} AS td_medium_ ,
+  td_campaign_${flag} AS td_campaign ,
+  IF(REGEXP_LIKE(td_campaign_${flag},'${td_flag_params}'),1,0) AS td_flag ,
   COUNT(*) AS order_cnt ,
   SUM(total_amount) AS total_amount
 FROM
